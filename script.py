c3118b0ae772e6c8741ca5c3f8efdce064460e8f
sharansonika923 <sonika.sharan@stackpro.in>
2023-01-20 10:36:48 +0530

    committed

---
+++ script.py
@@ -0,0 +1,35 @@
+import os
+
+# Create the Angular project
+os.system("ng new {{cookiecutter.projectname}}")
+
+# Navigate into the project directory
+os.chdir("{{cookiecutter.projectname}}")
+
+# Create the login component
+os.system("ng generate component login")
+
+# Create the signup component
+os.system("ng generate component signup")
+
+# Add routes for the login and signup components
+routes = """
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+"""
+
+with open("src/app/app-routing.module.ts", "w") as f:
+    f.write(routes)
