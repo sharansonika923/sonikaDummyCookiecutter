eb08dd9abab7268c4847f8a830a7484a994397c9
sharansonika923 <sonika.sharan@stackpro.in>
2023-01-20 12:22:52 +0530

    gg

--- {{cookiecutter.foldername}}/{{cookiecutter.filename}}.py
+++ {{cookiecutter.foldername}}/{{cookiecutter.filename}}.py
@@ -3,26 +3,6 @@ os.system("ng new {{cookiecutter.angularprojectname}}")
 os.chdir("{{cookiecutter.angularprojectname}}")
 os.system("ng generate component login")
 os.system("ng generate component signup")
-routes = """
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-
-const routes: Routes = [
-{ path: 'login', component: LoginComponent },
-{ path: 'signup', component: SignupComponent }
-];
-
-@NgModule({
-imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
-})
-export class AppRoutingModule { }
-"""
-
-with open("src/app/app-routing.module.ts", "w") as f:
-f.write(routes)
 
 login="""
 <form>
@@ -50,5 +30,110 @@ login="""
 </form>
 """
 with open("src/app/login/login.component.html", "w") as f:
-f.write(login)
+   f.write(login)
 
+logincss= """ /* Bordered form */
+form {
+  border: 3px solid #f1f1f1;
+}
+
+/* Full-width inputs */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+/* Add a hover effect for buttons */
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra style for the cancel button (red) */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the avatar image inside this container */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
+
+/* Avatar image */
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+}
+
+/* The "Forgot password" text */
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+    display: block;
+    float: none;
+  }
+  .cancelbtn {
+    width: 100%;
+  }
+} """
+with open("src/app/login/login.component.css", "w") as f:
+   f.write(logincss)
+
+apphtml="""
+<router-outlet></router-outlet>
+"""
+
+with open("src/app/app.component.css", "w") as f:
+   f.write(apphtml)
+
+
+routes = """
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+{ path: 'login', component: LoginComponent },
+{ path: 'signup', component: SignupComponent },
+ {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  }
+];
+
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
+"""
+
+with open("src/app/app-routing.module.ts", "w") as f:
+   f.write(routes)
