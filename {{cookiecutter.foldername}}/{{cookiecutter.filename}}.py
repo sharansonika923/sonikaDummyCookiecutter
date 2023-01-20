3474e4dfef45251695d4b0ef242cd8a37f56b8cd
sharansonika923 <sonika.sharan@stackpro.in>
2023-01-20 11:04:19 +0530

    ll

--- {{cookiecutter.foldername}}/{{cookiecutter.filename}}.py
+++ {{cookiecutter.foldername}}/{{cookiecutter.filename}}.py
@@ -1,26 +1,25 @@
-# import os
-# os.system("ng new {{cookiecutter.projectname}}")
-# os.chdir("{{cookiecutter.projectname}}")
-# os.system("ng generate component login")
-# os.system("ng generate component signup")
-# routes = """
-# import { NgModule } from '@angular/core';
-# import { Routes, RouterModule } from '@angular/router';
-# import { LoginComponent } from './login/login.component';
-# import { SignupComponent } from './signup/signup.component';
+import os
+os.system("ng new {{cookiecutter.projectname}}")
+os.chdir("{{cookiecutter.projectname}}")
+os.system("ng generate component login")
+os.system("ng generate component signup")
+routes = """
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-# const routes: Routes = [
-#   { path: 'login', component: LoginComponent },
-#   { path: 'signup', component: SignupComponent }
-# ];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
-# @NgModule({
-#   imports: [RouterModule.forRoot(routes)],
-#   exports: [RouterModule]
-# })
-# export class AppRoutingModule { }
-# """
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+"""
 
-# with open("src/app/app-routing.module.ts", "w") as f:
-#     f.write(routes)
-print("{{cookiecutter.projectname}}")
+with open("src/app/app-routing.module.ts", "w") as f:
+    f.write(routes)
